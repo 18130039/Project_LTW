e296f84c2192169484dfdc0cbfaae635d150fdf4
18130039 <80388171+18130039@users.noreply.github.com>
2022-12-20 06:36:35 +0700

    Tạo và hiện thực các phương thức trao đổi dữ liệu xuống db

---
+++ BachHoaXanh/src/main/java/dao/AccountDAO.java
@@ -0,0 +1,87 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import connectDB.ConnectDB;
+import model.Account;
+
+public class AccountDAO {
+	Connection con;
+	PreparedStatement ps;
+	ResultSet rs;
+
+	public AccountDAO() {
+
+	}
+
+	public Account checkLogin(String userName, String password) {
+		String query = "select * from account where userName=? and password=?";
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(query);
+			ps.setString(1, userName);
+			ps.setString(2, password);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				return new Account(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public Account checkAccountExist(String AccountName) {
+		String query = ("select * from Account where userName=?");
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(query);
+			ps.setString(1, AccountName);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				return new Account(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void register(String userName, String password, String name)
+			throws ClassNotFoundException {
+		String query = "INSERT INTO Account (userName, password, name, isSeller, isAdmin)" + "values(?,?,?,0,0);";
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(query);
+			ps.setString(1, userName);
+			ps.setString(2, password);
+			ps.setString(3, name);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void editAccount(String userName, String password, String name) throws ClassNotFoundException {
+		String query = "UPDATE Account SET userName=?, password=?, name=? WHERE userName=?;";
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(query);
+			ps.setString(1, userName);
+			ps.setString(2, password);
+			ps.setString(3, name);
+			ps.setString(4, userName);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		AccountDAO accountDAO = new AccountDAO();
+		System.out.println(accountDAO.checkLogin("admin", "admin"));
+	}
+}
---
+++ BachHoaXanh/src/main/java/dao/ProductDAO.java
@@ -0,0 +1,227 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import connectDB.ConnectDB;
+import model.Category;
+import model.Product;
+
+public class ProductDAO {
+	Connection con;
+	PreparedStatement ps;
+	ResultSet rs;
+
+	public List<Product> getAll() throws ClassNotFoundException {
+		List<Product> list = new ArrayList<>();
+		String sql = "SELECT * FROM Product;";
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				list.add(new Product(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5),
+						rs.getInt(6), rs.getString(7)));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+
+	}
+
+
+	public List<Product> getResult(String txtSearch) throws SQLException, ClassNotFoundException {
+		con = ConnectDB.getConnect();
+		List<Product> re = new LinkedList<>();
+		ResultSet rs = ps.executeQuery("select * from Product where pName like " + "'%" + txtSearch + "%'");
+		while (rs.next()) {
+			re.add(new Product(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5),
+					rs.getInt(6), rs.getString(7)));
+		}
+		return re;
+	}
+
+	public Product getById(String id) throws ClassNotFoundException {
+		try {
+			String sql = "select * from Product where pId=?";
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, id);
+
+			ResultSet rs = ps.executeQuery();
+			Product p = null;
+			while (rs.next()) {
+
+				p = new Product(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5),
+						rs.getInt(6), rs.getString(7));
+			}
+			rs.close();
+			ps.close();
+			return p;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public void deleteProduct(String pid) {
+		String sql = "delete from products where id = ?";
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, pid);
+			ps.executeUpdate();
+
+		} catch (Exception e) {
+		}
+	}
+
+	public List<Category> getAllCategory() {
+		List<Category> list = new LinkedList<>();
+		String sql = "select * from category";
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				list.add(new Category(rs.getString(1), rs.getString(2)));
+			}
+			rs.close();
+			ps.close();
+		} catch (Exception e) {
+		}
+		return list;
+	}
+
+	public List<Product> getProductByCid(String cid) {
+		List<Product> list = new LinkedList<>();
+		String sql = "select * from Product WHERE cateId=?";
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(sql);
+			ps.setString(1, cid);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				list.add(new Product(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5),
+						rs.getInt(6), rs.getString(7)));
+			}
+			rs.close();
+			ps.close();
+		} catch (Exception e) {
+		}
+		return list;
+	}
+
+	public void insertProduct(String id, String name, String img, String price, String priceLong, String quantity,
+			String cate) {
+		String query = "INSERT INTO Product (pId, pName, pImg, pPrice, pPriceLong, quantity, cateId  ) VALUES (?,?,?,?,?,?,?)";
+
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(query);
+			ps.setString(1, id);
+			ps.setString(2, name);
+			ps.setString(3, img);
+			ps.setString(4, price);
+			ps.setString(5, priceLong);
+			ps.setString(6, quantity);
+			ps.setString(7, cate);
+
+			ps.executeUpdate();
+
+		} catch (Exception e) {
+		}
+	}
+
+	public void insertOrder(String userName, String orderName, int total, String address, String phone) {
+		String query = "INSERT INTO Order (userName, orderName, totalMoney, address, phone) VALUES (?,?,?,?,?)";
+
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(query);
+			ps.setString(1, userName);
+			ps.setString(2, orderName);
+			ps.setInt(3, total);
+			ps.setString(4, address);
+			ps.setString(5, phone);
+			ps.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void editProduct(String id, String name, String img, String price, String priceLong, String cate) {
+		String query = "UPDATE Product SET pName=?, pImg=?, pPrice=?, pPriceLong=?, cateId=? WHERE pId=?;";
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(query);
+			ps.setString(1, name);
+			ps.setString(2, img);
+			ps.setString(3, price);
+			ps.setString(4, priceLong);
+			ps.setString(6, cate);
+			ps.setString(7, id);
+
+			ps.executeUpdate();
+
+		} catch (Exception e) {
+		}
+	}
+
+	public List<Product> searchByName(String txtSearch) {
+		List<Product> list = new ArrayList<>();
+		String query = "SELECT * FROM Product WHERE pName LIKE ?;";
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(query);
+			ps.setString(1, "%" + txtSearch + "%");
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				list.add(new Product(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5),
+						rs.getInt(6), rs.getString(7)));
+			}
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		return list;
+	}
+
+	public List<Product> get5Each(int index, int size) {
+		List<Product> list = new LinkedList<>();
+		String sql = "with x as(select *,ROW_NUMBER() over (ORDER by pId ASC) as r\n"
+				+ "                from Product)\n"
+				+ "                select * from x where r between (?*?-(?-1)) and (?*?)";
+
+		try {
+			con = ConnectDB.getConnect();
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, index);
+			ps.setInt(2, size);
+			ps.setInt(3, size);
+			ps.setInt(4, index);
+			ps.setInt(5, size);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				list.add(new Product(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5),
+						rs.getInt(6), rs.getString(7)));
+			}
+		} catch (Exception e) {
+		}
+		return list;
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		ProductDAO productDAO = new ProductDAO();
+		System.out.println(productDAO.getAll());
+
+	}
+}
